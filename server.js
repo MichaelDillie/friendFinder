0cc8ce980268e6d3591b15273873b03fae827558
Michael Dillie <michaeldillie@gmail.com>
2018-05-25 13:19:11 -0500

    Typo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PROT || 8088;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
